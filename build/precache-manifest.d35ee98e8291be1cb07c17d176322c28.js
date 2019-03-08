a724223b5081bb5a2526ce10747efe972fd7ad7e
mongru <monika.grubizna@gmail.com>
2019-03-08 17:00:51 +0000

    test production build

---
+++ build/precache-manifest.d35ee98e8291be1cb07c17d176322c28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "bcf4b0389816d3ee925d",
+    "url": "/static/js/main.7a243e02.chunk.js"
+  },
+  {
+    "revision": "944fc5e4e6c9261bdf5b",
+    "url": "/static/js/2.736300c1.chunk.js"
+  },
+  {
+    "revision": "bcf4b0389816d3ee925d",
+    "url": "/static/css/main.dd22c8e9.chunk.css"
+  },
+  {
+    "revision": "714dcbf463051b16e7625bbdfdb9548e",
+    "url": "/index.html"
+  }
+];

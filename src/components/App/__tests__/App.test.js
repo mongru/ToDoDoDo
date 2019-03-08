b61719012a72d7512d9a63ce7b12c4dedf39eb39
mongru <monika.grubizna@gmail.com>
2019-03-08 16:50:44 +0000

    add unit test for App component

---
+++ src/components/App/__tests__/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import App from '../App';
+import { initialState } from '../../../reducers/';
+
+configure({ adapter: new Adapter() })
+
+it('App renders without crashing', () => {
+  const mockFunction = jest.fn();
+
+  const component = shallow(
+    <App
+      state={initialState}
+      submitTodo={mockFunction}
+      todos={[]}
+    />,
+  );
+
+  expect(component.exists()).toEqual(true);
+});

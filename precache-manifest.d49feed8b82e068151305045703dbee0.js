8acabab249cba57fdb6e34f9960e49f398f65e2d
mongru <monika.grubizna@gmail.com>
2019-03-08 17:03:23 +0000

    Updates

---
+++ precache-manifest.d49feed8b82e068151305045703dbee0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd9486566d78d69fb6f5",
+    "url": "/ToDoDoDo/static/js/runtime~main.6226b65b.js"
+  },
+  {
+    "revision": "371a3e00508d28735ba4",
+    "url": "/ToDoDoDo/static/js/main.add2d370.chunk.js"
+  },
+  {
+    "revision": "944fc5e4e6c9261bdf5b",
+    "url": "/ToDoDoDo/static/js/2.736300c1.chunk.js"
+  },
+  {
+    "revision": "371a3e00508d28735ba4",
+    "url": "/ToDoDoDo/static/css/main.a56726d3.chunk.css"
+  },
+  {
+    "revision": "08598361e217f7ec30f1c723a12314dd",
+    "url": "/ToDoDoDo/index.html"
+  }
+];
